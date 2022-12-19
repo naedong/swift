761e8de5ae68fef8c1af44aa7119cd7150851e67
E4 <e4@E4ui-Macmini.local>
2022-12-19 11:37:13 +0900

    firest commit

---
+++ Examples/views/nav/NavView.swift
@@ -0,0 +1,20 @@
+//
+//  NavView.swift
+//  Examples
+//
+//  Created by E4 on 2022/12/19.
+//
+
+import SwiftUI
+
+struct NavView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavView()
+    }
+}
